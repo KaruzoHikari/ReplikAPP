248ea2b6675e92bd85427736f698e5b62b35474b
KaruzoDEV <35693714+KaruzoDEV@users.noreply.github.com>
2020-01-18 17:17:10 +0100

    Made tasks Async

--- LyokoAPP_Plugin/LyokoAPP_Plugin/DataPush.cs
+++ LyokoAPP_Plugin/LyokoAPP_Plugin/DataPush.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Threading.Tasks;
 using LyokoAPI.Events;
 using LyokoAPI.VirtualStructures.Interfaces;
 using LyokoAPI.VirtualStructures;
@@ -9,48 +10,52 @@ namespace LyokoAPP_Plugin
 {
     public static class DataPush
     {
-        public static string SendData(ITower tower)
+        public static void SendData(ITower tower)
         {
-            string dataKey = ServerKey.GetDataKey();
-
-            try
+            Task.Run(() =>
             {
-                var result = "-1";
-                var time = DateTime.Now;
-                var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-                int unixDateTime = (int) (time.ToUniversalTime() - epoch).TotalSeconds;
-                var webAddr = ("https://lyokoapp.firebaseio.com/" + Main.GetToken() + "/" + unixDateTime + ".json?auth=" + dataKey);
-                
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
-                httpWebRequest.Method = "PATCH";
-                httpWebRequest.ContentType = "application/json; charset=utf-8";
-                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                string dataKey = ServerKey.GetDataKey();
+
+                try
                 {
-                    var sName = Main.GetUppercaseNames(tower.Sector.Name);
-                    var data = new
+                    var result = "-1";
+                    var time = DateTime.Now;
+                    var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                    int unixDateTime = (int) (time.ToUniversalTime() - epoch).TotalSeconds;
+                    var webAddr = ("https://lyokoapp.firebaseio.com/" + Main.GetToken() + "/" + unixDateTime +
+                                   ".json?auth=" + dataKey);
+
+                    var httpWebRequest = (HttpWebRequest) WebRequest.Create(webAddr);
+                    httpWebRequest.Method = "PATCH";
+                    httpWebRequest.ContentType = "application/json; charset=utf-8";
+                    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                     {
-                        sector = sName,
-                        number = tower.Number.ToString(),
-                        activator = Enum.GetName(typeof(APIActivator), tower.Activator)
-                    };
-                    //var json = JsonConvert.SerializeObject(data);
-                    //streamWriter.Write(json);
-                    streamWriter.Flush();
-                }
+                        var sName = Main.GetUppercaseNames(tower.Sector.Name);
+                        var data = new
+                        {
+                            sector = sName,
+                            number = tower.Number.ToString(),
+                            activator = Enum.GetName(typeof(APIActivator), tower.Activator)
+                        };
+                        //var json = JsonConvert.SerializeObject(data);
+                        //streamWriter.Write(json);
+                        streamWriter.Flush();
+                    }
 
-                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        result = streamReader.ReadToEnd();
+                    }
+
+                    return result;
+                }
+                catch (Exception e)
                 {
-                    result = streamReader.ReadToEnd();
+                    LyokoLogger.Log("LyokoAPP", e.ToString());
+                    return "SomethingWrong";
                 }
-
-                return result;
-            }
-            catch (Exception e)
-            {
-                LyokoLogger.Log("LyokoAPP", e.ToString());
-                return "SomethingWrong";
-            }
+            });
         }
     }
 }
--- LyokoAPP_Plugin/LyokoAPP_Plugin/FireBasePush.cs
+++ LyokoAPP_Plugin/LyokoAPP_Plugin/FireBasePush.cs
@@ -1,48 +1,54 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Threading.Tasks;
 using LyokoAPI.Events;
 
 namespace LyokoAPP_Plugin
 {
     public class FireBasePush
     {
-        public static string SendMessage(string title, string body)
+        public static void SendMessage(string title, string body)
         {
-            string serverKey = ServerKey.GetServerKey();
-
-            try
+            Task.Run(() =>
             {
-                var result = "-1";
-                var webAddr = "https://fcm.googleapis.com/fcm/send";
+                string serverKey = ServerKey.GetServerKey();
 
-                var regId = Main.GetToken();
-
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
-                httpWebRequest.ContentType = "application/json";
-                httpWebRequest.Headers.Add("Authorization:key=" + serverKey);
-                httpWebRequest.Method = "POST";
-
-                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                try
                 {
-                    string json = "{\"to\": \"" + regId + "\",\"notification\": {\"title\": \"" + title + "\",\"body\": \"" + body + "\"}}";
-                    streamWriter.Write(json);
-                    streamWriter.Flush();
-                }
+                    var result = "-1";
+                    var webAddr = "https://fcm.googleapis.com/fcm/send";
 
-                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream() ?? throw new InvalidOperationException()))
+                    var regId = Main.GetToken();
+
+                    var httpWebRequest = (HttpWebRequest) WebRequest.Create(webAddr);
+                    httpWebRequest.ContentType = "application/json";
+                    httpWebRequest.Headers.Add("Authorization:key=" + serverKey);
+                    httpWebRequest.Method = "POST";
+
+                    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                    {
+                        string json = "{\"to\": \"" + regId + "\",\"notification\": {\"title\": \"" + title +
+                                      "\",\"body\": \"" + body + "\"}}";
+                        streamWriter.Write(json);
+                        streamWriter.Flush();
+                    }
+
+                    var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
+                    using (var streamReader =
+                        new StreamReader(httpResponse.GetResponseStream() ?? throw new InvalidOperationException()))
+                    {
+                        result = streamReader.ReadToEnd();
+                    }
+
+                    return result;
+                }
+                catch (Exception e)
                 {
-                    result = streamReader.ReadToEnd();
+                    LyokoLogger.Log("LyokoAPP", e.ToString());
+                    return "SomethingWrong";
                 }
-
-                return result;
-            }
-            catch (Exception e)
-            {
-                LyokoLogger.Log("LyokoAPP", e.ToString());
-                return "SomethingWrong";
-            }
+            });
         }
     }
 }
